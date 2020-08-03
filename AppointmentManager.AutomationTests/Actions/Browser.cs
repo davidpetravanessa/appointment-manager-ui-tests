02e008e2279b07e60017eac3168207213f88274a
Petra Vanessa David <david.petra.pd@gmail.com>
2020-08-03 20:56:34 +0200

    added browser actions

---
+++ AppointmentManager.AutomationTests/Actions/Browser.cs
@@ -0,0 +1,26 @@
+﻿using AppointmentManager.AutomationTests.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppointmentManager.AutomationTests.Actions
+{
+    public class Browser
+    {
+        public static void NavigateTo(string url)
+        {
+            WebDriver.Driver.Navigate().GoToUrl(url);
+        }
+
+        public static void MaximizeWindow()
+        {
+            WebDriver.Driver.Manage().Window.Maximize();
+        }
+
+        public static void Quit()
+        {
+            WebDriver.Quit();
+        }
+    }
+}
