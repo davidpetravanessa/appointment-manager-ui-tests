81f9dca937c7547b0e2d1d2bad7da4edee48ffcb
Petra Vanessa David <david.petra.pd@gmail.com>
2020-08-03 20:47:45 +0200

    -added Encryption method -some utilities (Explicit wait; findElemWait) -prepared app settings

---
+++ AppointmentManager.AutomationTests/Helpers/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using AppointmentManager.AutomationTests.Base;
+using System;
+using System.Configuration;
+
+namespace AppointmentManager.AutomationTests.Helpers
+{
+    public class AppSettings
+    {
+        private static Configuration _configuration => ConfigurationManager.OpenExeConfiguration(@"AppointmentManager.AutomationTests.dll");
+        public static BrowserType BrowserType => (BrowserType)Enum.Parse(typeof(BrowserType), _configuration.AppSettings.Settings["BrowserType"].Value);
+        public static string BaseUrl => _configuration.AppSettings.Settings["BaseUrl"].Value;
+        public static string UserName => AesEncryptionService.Decrypt(_configuration.AppSettings.Settings["UserName"].Value);
+        public static string Password => AesEncryptionService.Decrypt(_configuration.AppSettings.Settings["Password"].Value);
+
+    }
+}
