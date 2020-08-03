6f0a605cf7183dbbf8cde7cca456c22967a353a4
Petra Vanessa David <david.petra.pd@gmail.com>
2020-08-03 20:53:15 +0200

    -added webdriver methods -navigation method -chrome browsertype

---
+++ AppointmentManager.AutomationTests/Actions/Navigation.cs
@@ -0,0 +1,12 @@
+﻿using AppointmentManager.AutomationTests.Helpers;
+
+namespace AppointmentManager.AutomationTests.Actions
+{
+    public class Navigation
+    {
+        public static void NavigateTo()
+        {
+            Browser.NavigateTo(AppSettings.BaseUrl);
+        }
+    }
+}
---
+++ AppointmentManager.AutomationTests/Base/WebDriver.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Diagnostics;
+using System.IO;
+using AppointmentManager.AutomationTests.Helpers;
+
+namespace AppointmentManager.AutomationTests.Base
+{
+    public class WebDriver
+    {
+        private static IWebDriver _webDriver;
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (_webDriver == null)
+                {
+                    _webDriver = GetWebDriver();
+                }
+                return _webDriver;
+            }
+        }
+
+        public static void Quit()
+        {
+            _webDriver?.Close();
+            _webDriver?.Quit();
+
+            Process[] chromeDriverProcesses = Process.GetProcessesByName("chromedriver");
+
+            foreach (var chromeDriverProcess in chromeDriverProcesses)
+            {
+                chromeDriverProcess.Kill();
+            }
+            _webDriver = null;
+        }
+
+        private static IWebDriver GetWebDriver()
+        {
+            switch (AppSettings.BrowserType)
+            {
+                case BrowserType.Chrome:
+                    var extensionPath = $"{Directory.GetCurrentDirectory()}\\Base\\AuthExtension";
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArgument($@"--load-extension={extensionPath}");
+                    return new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory, chromeOptions, TimeSpan.FromSeconds(180));
+                case BrowserType _browserType: throw new NotSupportedException($"{_browserType} is not a supported browser");
+                default: throw new NotSupportedException("not supported browser: <null>");
+            }
+        }
+    }
+}
